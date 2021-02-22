1ad9703329046db994b0cc003bf2f5d0e66b90eb
Senade Loko <lokosenade@gmail.com>
2021-02-22 10:28:18 +0100

    first commit

---
+++ employe/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {HttpModule} from '@angular/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
